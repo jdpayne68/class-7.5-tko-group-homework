0a46336c7bd3a7532fb57ad5967a3c2d9c8bbbb1
kilik42 <marvinevins@yahoo.com>
2026-04-21 20:37:26 -0500

    added main vpc

---
+++ terraform2/vpc.tf
@@ -0,0 +1,43 @@
+# STAGE 2: VPC FOUNDATION
+# Goal:
+# - Enable required APIs
+# - Create custom VPC
+#
+# Verify:
+# - Compute API enabled
+# - VPC named "main" exists
+#
+# Screenshot required:
+# - API page
+# - VPC Networks page
+
+
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_service
+# Enable the Compute API 
+resource "google_project_service" "compute" {
+  
+  service = "compute.googleapis.com"
+  disable_on_destroy = false
+}
+
+
+# Enable the GKE API
+resource "google_project_service" "container" {
+  service = "container.googleapis.com"
+  disable_on_destroy = false
+}
+
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network 
+resource "google_compute_network" "main" {
+  name                            = "main"
+  routing_mode                    = "REGIONAL"
+  auto_create_subnetworks         = false
+  mtu                             = 1460
+  delete_default_routes_on_create = false
+
+  # Make sure the required APIs are enabled before creating the network
+  depends_on = [
+    google_project_service.compute,
+    google_project_service.container
+  ]
+}
