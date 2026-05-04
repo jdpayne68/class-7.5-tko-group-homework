7f30f7d0206d86179b1222d315f3d0b85661f503
KingBuu838 <229227597+KingBuu838@users.noreply.github.com>
2026-05-03 22:08:18 -0400

    homework

---
+++ 00-provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "7.29.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "project8338-490201"
+  region  = "us-central1"
+}
