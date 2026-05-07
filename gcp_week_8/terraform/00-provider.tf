a2218bc7be7c33d3d42ad2fdb47a8d1e7f20e4bd
KingBuu838 <229227597+KingBuu838@users.noreply.github.com>
2026-05-07 15:11:15 -0400

    first commit

--- gcp_week_8/terraform/00-provider.tf
+++ gcp_week_8/terraform/00-provider.tf
@@ -1,8 +1,9 @@
 terraform {
+  required_version = ">= 1.10"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "7.29.0"
+      version = "7.31.0" #current version as of 5/7/2026
     }
   }
 }
